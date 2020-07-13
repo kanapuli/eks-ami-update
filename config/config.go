387a4a90144bede047f520ef5535b38f723aef08
kanapuli <athavankanapuli@gmail.com>
2020-07-13 09:30:33 +0530

    Add New() and Read() func in config package
    New initializes a configuration struct
    Read reads a json file and applies to configuration struct
    Add tests for config.Read function

--- config/config.go
+++ config/config.go
@@ -1,10 +1,32 @@
 package config
 
-//Configuration is  the application configuration
+import (
+	"encoding/json"
+	"io"
+)
+
+// Configuration is the base application configuration
 type Configuration struct {
 	AutoscalingEnabled  bool   `json:"autoscalingEnabled"`
 	AutoscalerNamespace string `json:"autoscalerNamespace"`
 	AsgName             string `json:"asgName"`
 	AsgDesiredCapacity  int    `json:"asgDesiredCapacity"`
 	DryRun              bool   `json:"dryRun"`
+}
+
+// New returns a new instance of Configuration
+func New() *Configuration {
+	return &Configuration{
+		AutoscalingEnabled:  false,
+		AutoscalerNamespace: "default",
+		AsgName:             "",
+		AsgDesiredCapacity:  0,
+		DryRun:              true,
+	}
+}
+
+// Read reads config file and populates Configuration struct
+func (c *Configuration) Read(file io.Reader) error {
+	json.NewDecoder(file).Decode(&c)
+	return nil
 }
---
+++ config/config_test.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestRead(t *testing.T) {
+	jsonInput := strings.NewReader("{ \"autoscalingEnabled\": true, \"autoscalerNamespace\": \"default\", \"asgName\": \"eks-1-16-asg\" , \"asgDesiredCapacity\": 2, \"dryRun\": false }")
+	conf := new(Configuration)
+	err := conf.Read(jsonInput)
+	if err != nil {
+		t.Errorf("Expected Json parsing but got %v", err)
+	}
+	if conf.AsgDesiredCapacity != 2 || conf.AutoscalingEnabled != true || conf.DryRun != false || conf.AutoscalerNamespace != "default" || conf.AsgName != "eks-1-16-asg" {
+		t.Errorf("Json parsing values expected: %v but got %v", jsonInput, conf)
+	}
+}
