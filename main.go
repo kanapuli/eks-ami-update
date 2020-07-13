a226c3b1bff4e561653a4cee338baf533b78883c
kanapuli <athavankanapuli@gmail.com>
2020-07-13 09:29:13 +0530

    Remove config parsing logic from main function

--- main.go
+++ main.go
@@ -1,23 +1,4 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"os"
-
-	"github.com/kanapuli/eks-ami-update/config"
-)
-
 func main() {
-	//read config file
-	file, err := os.Open("config.json")
-	if err != nil {
-		log.Fatalf("Unable to find config file: %v", err)
-		os.Exit(1)
-	}
-	decoder := json.NewDecoder(file)
-	cfgFile := new(config.Configuration)
-	decoder.Decode(&cfgFile)
-	//	fmt.Println(cfgFile)
-
 }
