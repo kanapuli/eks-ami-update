c0a76ed0d52128034e518b7333b4815d7e45cb69
kanapuli <athavankanapuli@gmail.com>
2020-07-14 07:37:43 +0530

    Read and parse config.json from main func
    Uses config package functions to read  the config.json file and
    parse the values to the Configuration struct.

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/kanapuli/eks-ami-update/config"
+)
+
+const (
+	configFile = "config.json"
+)
+
 func main() {
+
+	// Read and parse config file
+	file, err := os.Open(configFile)
+	if err != nil {
+		log.Printf("Expected opening file %v but got %v", configFile, err)
+		os.Exit(1)
+	}
+	cfg := config.New()
+	err = cfg.Read(file)
+	if err != nil {
+		log.Printf("Expected json parsing for config file %v but got %v", configFile, err)
+		os.Exit(1)
+	}
 }
